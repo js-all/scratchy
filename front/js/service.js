c250e21fb44ae8c151bd26328e84023c8669b3d6
js-all <titouangoulois29@gmail.com>
2021-05-15 16:36:48 +0200

    progress, scratchy servcice is now a js class

--- front/js/service.js
+++ front/js/service.js
@@ -1,10 +1,9 @@
 /*
 - add axios cdn link in index.html
-     	<scritp src="https://unpkg.com/axios@0.21.1/dist/axios.min.js"></scritp>
+     	<script src="https://unpkg.com/axios@0.21.1/dist/axios.min.js"></script>
 - start scratchy service : 
-    var api = new ScratchyService("http://localhost:5000/api");
-    api;
-- call a function :
+    const api = new ScratchyService("http://localhost:5000/api");
+- call a method :
     api.function();|
 */
 
@@ -13,114 +12,121 @@
  * @constructor
  * @param {string} apiUrl - Scratchy service API URL, eg: "http://localhost:5000"
  */
- function ScratchyService(apiUrl) {
-    let _this = this;
-    this.cacheUsers = []; // List of users, used as cache
-
-//                               ROOM FUNCTIONS
+class ScratchyService {
     /**
+     * create a new ScratchyService
+     * @param {string} apiUrl the api's url (without the trailing /)
+     */
+    constructor(apiUrl) {
+        this.apiUrl = apiUrl;
+        this.cachedUsers = [];
+    }
+
+    /**
+     * update a room
      * @param {string} roomID - room id , eg:60895dd62d1a706830c31f10
      * @param {string} roomTitle - title of a room , eg:my room
      * @param {string} roomDescription - description of a room, eg:my room description
-     * @returns {string} - room id
+     * @returns {object} a json representation of the room
      */
-    this.updateRoom = async function(roomId,roomTitle,roomDescription){
-        const reponse = await axios.put(apiUrl+'/room/'+roomId, { title: roomTitle , description: roomDescription });
+    async updateRoom(roomId, roomTitle, roomDescription){
+        const reponse = await axios.put(this.apiUrl + '/room/' + roomId, { title: roomTitle , description: roomDescription });
         return reponse.data;
     };
 
+
     /**
-     * 
+     * create a room
      * @param {string} roomTitle - title of a room, eg:my room
      * @param {string} roomDescription - description of a room, eg:my room description
      * @returns {string} - room id 
      */
-    this.createRoom = async function(roomTitle,roomDescription){
-        const reponse = await axios.post(apiUrl+'/room', { title: roomTitle , description: roomDescription });
+    async createRoom(roomTitle, roomDescription){
+        const reponse = await axios.post(this.apiUrl+'/room', { title: roomTitle , description: roomDescription });
         return reponse.data;
     };
 
     /**
-     * 
+     * delete a room
      * @param {string} roomID - room id , eg: 60895dd62d1a706830c31f10
      */
-    this.deleteRoom = async function(roomID){
-        const reponse = await axios.delete(apiUrl+'/room/'+roomID);
+    async deleteRoom(roomID){
+        const reponse = await axios.delete(this.apiUrl+'/room/'+roomID);
         return reponse.data;
     };
     /**
-     * 
+     * get every room on the database
      * @returns {array} - all rooms information 
      */
-    this.getAllRooms = async function(){ // put the id between "" 
-        let reponse = await axios.get(apiUrl+'/room'); // make the GET request;
+    async getAllRooms() { // put the id between "" 
+        let reponse = await axios.get(this.apiUrl+'/room'); // make the GET request;
         return reponse.data;
     };
 
     /**
-     * 
+     * retreive a room's data
      * @param {string} roomID - room id , eg:60895dd62d1a706830c31f10
      * @returns {JSON} room information , { oid:"60895dd62d1a706830c31f10" ,title:"example"}
      */
-    this.getRoom = async function(roomID){ // put the id between "" 
+    async getRoom(roomID) { // put the id between "" 
             // Make a request for a user with a given ID
-            let reponse = await axios.get(apiUrl+'/room/'+roomID);// make the GET request
+            let reponse = await axios.get(this.apiUrl+'/room/'+roomID);// make the GET request
             return reponse.data;// return data JSON           
     };
 
 //                               USER FUNCTIONS
     
     /**
-     * 
+     * get a user by their pseudo
      * @param {string} userPseudo - a user pseudo , eg:toto
      * @returns {JSON} - user information 
      */
-    this.getUserByPseudo = async function(userPseudo){ // put the id between "" 
+    async getUserByPseudo(userPseudo){ // put the id between "" 
         // Make a request for a user with a given ID
-        let reponse = await axios.get(apiUrl+'/user?pseudo='+userPseudo);// make the GET request
+        let reponse = await axios.get(this.apiUrl+'/user?pseudo='+userPseudo);// make the GET request
         return reponse.data;// return data JSON         
     };
 
     /**
-     * 
+     * get user by their id
      * @param {string} userID , user id , eg:60895dd62d1a706830c31f10
      * @returns {JSON} - user information
      */
-    this.getUserByid = async function(userID){ // put the id between "" 
+    async getUserByid (userID){ // put the id between "" 
         // Make a request for a user with a given ID
-        let reponse = await axios.get(apiUrl+'/user/'+userID) // make the GET request
+        let reponse = await axios.get(this.apiUrl+'/user/'+userID) // make the GET request
         return reponse.data;// return data JSON          
     };
 
     /**
-     * 
+     * create user
      * @param {string} userPseudo - a user pseudo , eg:toto
-     * @param {*} userProfileImage - profile image link , eg:https://myprofileimage.test/picture.png
-     * @returns {string} - user id , eg: 60895dd62d1a706830c31f10
+     * @param {string} userProfileImage - profile image link , eg:https://myprofileimage.test/picture.png
+     * @returns {object} returns a json representation of the user
      */
-    this.createUser = async function(userPseudo,userProfileImage){
-        const reponse = await axios.post(apiUrl+'/user', { pseudo: userPseudo , profileImage: userProfileImage });
+    async createUser(userPseudo, userProfileImage){
+        const reponse = await axios.post(this.apiUrl+'/user', { pseudo: userPseudo , profileImage: userProfileImage });
         return reponse.data;
     };
 
     /**
-     * 
+     * delete user
      * @param {string} userID - user id , eg:60895dd62d1a706830c31f10
      */
-    this.deleteUser = async function(userID){
-        // 
-        const reponse = await axios.delete('http://localhost:5000/api/user/'+userID);
+    async deleteUser(userID){
+        const reponse = await axios.delete(this.apiUrl+'/user/'+userID);
         return reponse.data;
     };
 
     /**
-     * 
+     * update a user
      * @param {string} userID - user id , eg:60895dd62d1a706830c31f10
      * @param {string} userPseudo - user pseudo , eg:toto
+     * @param {string} userProfileImage - user profile image
      * @returns {string} - user id , eg:60895dd62d1a706830c31f10
      */
-    this.updateUser = async function(userID,userPseudo){
-        const reponse = await axios.put(apiUrl+'/user/'+userID, { pseudo: userPseudo});
+    async updateUser(userID, userPseudo, userProfileImage){
+        const reponse = await axios.put(this.apiUrl+'/user/'+userID, { pseudo: userPseudo, profileImage: userProfileImage});
         return reponse.data;
     };
 
@@ -131,9 +137,9 @@
      * @param {string} messageID - message id , eg:60895dd62d1a706830c31f10
      * @returns {JSON} message information 
      */
-    this.getMessage = async function(messageID){ // put the id between "" 
+    async getMessage(messageID){ // put the id between "" 
         // Make a request for a user with a given ID
-        let reponse = await axios.get(apiUrl+'/message/'+messageID) // make the GET request
+        let reponse = await axios.get(this.apiUrl+'/message/'+messageID) // make the GET request
         return reponse.data;// return data JSON  
 
     };
@@ -143,9 +149,9 @@
      * @param {string} roomID - room id , eg:60895dd62d1a706830c31f10
      * @returns {array} message information
      */
-    this.getAllMessagesInRoom = async function(roomID){ // put the id between "" 
+    async getAllMessagesInRoom(roomID){ // put the id between "" 
         // Make a request for a user with a given ID
-        let reponse = await axios.get(apiUrl+'/message?roomid='+roomID) // make the GET request
+        let reponse = await axios.get(this.apiUrl+'/message?roomid='+roomID) // make the GET request
         return reponse.data;// return data JSON 
     };
 
@@ -154,10 +160,10 @@
      * @param {string} authorID - author id , eg:60895dd62d1a706830c31f10
      * @param {string} messageContent - message content , eg:my message content
      * @param {string} roomID - room id , eg:60895dd62d1a706830c31f10
-     * @returns {string} message id
+     * @returns {object} returns a json representation of the message
      */
-    this.createMessage = async function(authorID,messageContent,roomID){
-        const reponse = await axios.post(apiUrl+'/message', { author : authorID , content : messageContent , roomId : roomID });
+    async createMessage(authorID,messageContent,roomID){
+        const reponse = await axios.post(this.apiUrl+'/message', { author : authorID , content : messageContent , roomId : roomID });
         return reponse.data;
     };
 
@@ -167,8 +173,8 @@
      * @param {string} messageContent - message content , eg:my message content 
      * @returns {string} message id
      */
-    this.updateMessage = async function(messageID,messageContent){
-        const reponse = await axios.put(apiUrl+'/message/'+messageID, { content: messageContent });
+    async updateMessage(messageID,messageContent){
+        const reponse = await axios.put(this.apiUrl+'/message/'+messageID, { content: messageContent });
         return reponse.data;
     };
 
@@ -176,8 +182,8 @@
      * 
      * @param {string} messageID - message id , eg:60895dd62d1a706830c31f10
      */
-    this.deleteMessage = async function(messageID){
-        const reponse = await axios.delete(apiUrl+'/message/'+messageID);
+    async deleteMessage(messageID){
+        const reponse = await axios.delete(this.apiUrl+'/message/'+messageID);
         return reponse.data;
     };
-};
+}
